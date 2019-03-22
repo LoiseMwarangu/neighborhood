7cfab0b2124859490f430f930ae59c19f9e1e554
Loise Mwarangu <loisemwarangu@gmail.com>
2019-03-22 16:18:46 +0300

    deployment

--- hood/models.py
+++ hood/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from django.db import models
+from tinymce.models import HTMLField
+from django.core.validators import URLValidator
+from django.contrib.auth.models import User
 
+class User(models.Model):
+
+    name = models.ForeignKey(User,on_delete=models.CASCADE, null = True)
+    email = models.ImageField(upload_to = 'profile/')
+    bio = models.TextField(max_length=500)
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
